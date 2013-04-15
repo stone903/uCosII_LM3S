d05e6da81ed60720cf2a226bd0d834c7b5bbeeeb
Lai Yongqiang <laiyongqiang@gmail.com>
2013-04-16 00:10:50 +0800

    Add Test Output for test.

--- BSP/bsp.c
+++ BSP/bsp.c
@@ -7,32 +7,61 @@
 
 #include "includes.h"
 
+/* SysTick output for test */
 INT32U SysTickOut;
+INT32U SysTickLedOut;
 
+/* This Function is to initialize and test the system clock and GPIOs. */
 void Board_Init(void)
 {
     // 配置系统主时钟, 使用外部晶振16M
     SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_XTAL_16MHZ |
                    SYSCTL_OSC_MAIN);
 
-    // 使能端口PB，然后配置PB.4为输出
+    // 使能端口PB，然后配置PB.456为输出
     SysCtlPeripheralEnable(GPIO_LED_SYSCTL_PERIPH);
-    GPIOPinTypeGPIOOutput(GPIO_LED_PORT_BASE, GPIO_LED_PIN);
+    GPIOPinTypeGPIOOutput(GPIO_LED_PORT_BASE, GPIO_LED_PIN1);
+	GPIOPinTypeGPIOOutput(GPIO_LED_PORT_BASE, GPIO_LED_PIN2);
+	GPIOPinTypeGPIOOutput(GPIO_LED_PORT_BASE, GPIO_LED_PIN3);
 
     // 置PB.4为高电平
-    GPIOPinWrite(GPIO_LED_PORT_BASE, GPIO_LED_PIN, GPIO_LED_PIN);
+    GPIOPinWrite(GPIO_LED_PORT_BASE, GPIO_LED_PIN1, GPIO_LED_PIN1);
 
     // 延时500ms
     SysCtlDelay(5000 * SysCtlClockGet()/3000);
 
     // 置PB.4为低电平
-    GPIOPinWrite(GPIO_LED_PORT_BASE, GPIO_LED_PIN, 0);
+    GPIOPinWrite(GPIO_LED_PORT_BASE, GPIO_LED_PIN1, ~GPIO_LED_PIN1);
 
     // 延时500ms
     SysCtlDelay(5000 * SysCtlClockGet()/3000);
 
+	// 置PB.4为高电平
+    GPIOPinWrite(GPIO_LED_PORT_BASE, GPIO_LED_PIN2, GPIO_LED_PIN2);
+
+    // 延时500ms
+    SysCtlDelay(5000 * SysCtlClockGet()/3000);
+
+    // 置PB.4为低电平
+    GPIOPinWrite(GPIO_LED_PORT_BASE, GPIO_LED_PIN2, ~GPIO_LED_PIN2);
+
+    // 延时500ms
+    SysCtlDelay(5000 * SysCtlClockGet()/3000);
+
+	// 置PB.4为高电平
+    GPIOPinWrite(GPIO_LED_PORT_BASE, GPIO_LED_PIN3, GPIO_LED_PIN3);
+
+    // 延时500ms
+    SysCtlDelay(5000 * SysCtlClockGet()/3000);
+
+    // 置PB.4为低电平
+    GPIOPinWrite(GPIO_LED_PORT_BASE, GPIO_LED_PIN3, ~GPIO_LED_PIN3);
+
+    // 延时500ms
+    SysCtlDelay(5000 * SysCtlClockGet()/3000);
 }
 
+/* This function is to initialize the SysTick relate configurations */
 void BSP_SysTickInit()
 {
 	OS_ENTER_CRITICAL();
@@ -44,9 +73,24 @@ void BSP_SysTickInit()
 	OS_EXIT_CRITICAL();
 
 	SysTickOut = 0x01;
-	//Enable SYS_TICK_OUTPUT to Test
-	SysCtlPeripheralEnable(GPIO_PORTD_BASE);
-	GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE,GPIO_SYS_TICK_OUTPUT);
+	SysTickLedOut = GPIO_LED_PIN1;
 }
 
+/* This function is initialize the priority fo PendSV for context switch */
+void BSP_PENDSVSetPrio(unsigned char ucPriority)
+{
+	IntPrioritySet(FAULT_PENDSV,ucPriority);
+}
+
+/* This function is to initialize the Test GPIOs to Output */
+void BSP_TestOutPutInit()
+{
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
+	GPIOPinTypeGPIOOutput(GPIO_TEST_OUT_BASE,GPIO_SYS_TICK_OUTPUT);
+	GPIOPinTypeGPIOOutput(GPIO_TEST_OUT_BASE,GPIO_TEST1_OUTPUT);
+	GPIOPinTypeGPIOOutput(GPIO_TEST_OUT_BASE,GPIO_TEST2_OUTPUT);
+	GPIOPinTypeGPIOOutput(GPIO_TEST_OUT_BASE,GPIO_TEST3_OUTPUT);
+	GPIOPinTypeGPIOOutput(GPIO_TEST_OUT_BASE,GPIO_TEST4_OUTPUT);
+	GPIOPinTypeGPIOOutput(GPIO_TEST_OUT_BASE,GPIO_TEST5_OUTPUT);
+}
 
--- BSP/bsp.h
+++ BSP/bsp.h
@@ -5,13 +5,27 @@
  *      Author: Stone
  */
 
+/* LED PIN Definition */
 #define GPIO_LED_SYSCTL_PERIPH  SYSCTL_PERIPH_GPIOB
 #define GPIO_LED_PORT_BASE      GPIO_PORTB_BASE
-#define GPIO_LED_PIN            (GPIO_PIN_4)
+
+#define GPIO_LED_PIN1            (GPIO_PIN_4)
+#define GPIO_LED_PIN2            (GPIO_PIN_5)
+#define GPIO_LED_PIN3            (GPIO_PIN_6)
+#define GPIO_LED_PIN4            (GPIO_PIN_7)
+
+/* Test Port Definition */
 #define GPIO_TEST_OUT_BASE  	(GPIO_PORTD_BASE)
 #define GPIO_SYS_TICK_OUTPUT    (GPIO_PIN_0)
+#define GPIO_TEST1_OUTPUT    (GPIO_PIN_1)
+#define GPIO_TEST2_OUTPUT    (GPIO_PIN_2)
+#define GPIO_TEST3_OUTPUT    (GPIO_PIN_3)
+#define GPIO_TEST4_OUTPUT    (GPIO_PIN_4)
+#define GPIO_TEST5_OUTPUT    (GPIO_PIN_5)
 
+/* BSP Relate Functions */
 void Board_Init();
 void BSP_SysTickInit();
+void BSP_PENDSVSetPrio(unsigned char ucPriority);
 
 
